72df387438a127d4a40e56288176520b85dfde7f
hyunsoo <hjames2000@snu.ac.kr>
2023-01-29 18:19:30 +0900

    asd

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
-import sys
-input=sys.stdin.readline
+import math
 
-x=input().replace('\n', '1')
-y=input()
+while 1:
+    print("비행기 기종을 입력해주세요(-1 입력시 프로그램 종료): ", end='')
+    x=input()
+    if x=='A300':
+        while 1:
+            dist=int(input())
+            if dist==-1:
+                break
+            print(round(-9.452*math.log(dist)+89.688))
+    elif x=='B737':
+        while 1:
+            dist=int(input())
+            if dist==-1:
+                break
+            print(round(0.000002*dist*dist-0.014*dist+36.421))
+    elif x=='-1':
+        break
+    else:
+        print("다시 입력해주세요.\n")
 
-print(x)
-print(y)
+
+
+    
