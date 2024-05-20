f5ca97f3fb44e27a12bd26457cb0fec0411c468d
kage3175 <hjames2000@snu.ac.kr>
2024-05-20 14:40:57 +0900

    temp, temp2

--- temp.py
+++ temp.py
@@ -1,18 +1,4 @@
+import os
 import sys
-sys.setrecursionlimit(10**7)
-VAL = 0
 
-
-def algB(n):
-  #print("n: ", n)
-  if n <= 1 :
-    return 1
-  else:
-    tmp = 0
-    t = algB(n // 3)
-    for i in range(1, t + 1):
-      tmp += 1
-    tmp = tmp + algB((2 * n) // 3)
-    return tmp
-  
-print(algB(10000))
+os.system("python temp2.py 1")
