21826fb30d10c455f2ab04e3fb480235cdf2e750
kage3175 <hjames2000@snu.ac.kr>
2024-05-25 12:43:18 +0900

    color modes updated

--- temp.py
+++ temp.py
@@ -1,4 +1,17 @@
-import os
-import sys
+import networkx as nx
 
-os.system("python temp2.py 1")
+
+def build_knowledge_graph(concepts):
+  try:
+      G = nx.DiGraph()
+      for i, concept in enumerate(concepts[:20]):
+          G.add_node(concept, label=concept)
+          if i > 0:
+              G.add_edge(concepts[i-1], concept)
+      return G
+  except Exception as e:
+      print("X")
+      
+g = build_knowledge_graph([1,2,3])
+      
+print(g.edges)
